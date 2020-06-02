712c8d2e82b22cc130de7dff2576ee251fae27d8
Shrey Sachdeva <shreysachdeva.2000@gmail.com>
2020-06-03 01:57:44 +0530

    feat(TrendingPalette): add trending palette section
    Signed-off-by: Shrey Sachdeva <shreysachdeva.2000@gmail.com>

---
+++ src/components/TrendingPalette.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+import data from "../utils/default/trending.json";
+
+const TrendingPalette: React.FC = () => {
+  const trendingPalettes = data.palettes;
+  const palette = Object.values(trendingPalettes);
+  return (
+    <div>
+      <h4 className="kz-title">Trending Palettes</h4>
+      {palette.map((colors) => {
+        return colors.map((color) => (
+            <div
+              className="kz-trending-palette"
+              style={{ backgroundColor: `${color}` }}
+            />
+        ));
+      })}
+      <style jsx>{`
+        .kz-title {
+          color: #181d20;
+          margin: 10px !important;
+          margin-top: 30px !important;
+        }
+        .kz-palette-container {
+          display: inline-block;
+        }
+        .kz-trending-palette {
+          height: 20px;
+          width: 20px;
+          border-radius: 5px;
+          display: inline-block;
+        }
+      `}</style>
+    </div>
+  );
+};
+
+export default TrendingPalette;
